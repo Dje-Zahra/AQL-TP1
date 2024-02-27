bb811d50aa3540d8f227b1c9a28b9cd8a3be4189
Zahra <zahra.25.djedaidi@gmail.com>
2024-02-27 10:23:25 -0800

    2

---
+++ src/test/java/BinarySearchTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeEach;
+
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTest {
+
+    @BeforeEach
+    void init(){
+        int[] array = new int[0];
+        array = [4,5,6,8,2,9,10];
+    }
+
+}
